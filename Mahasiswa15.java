5126f6c341dd7d39714602dc6f3ff7be4e30982a
Furqon Adzikri <furqonadzikri311@gmail.com>
2026-02-25 22:20:48 +0700

    Percobaan 3 Konstruktor

--- Mahasiswa15.java
+++ Mahasiswa15.java
@@ -4,6 +4,18 @@ public class Mahasiswa15 {
     String kelas;
     double ipk;
 
+    // Konstruktor default
+    public Mahasiswa15() {
+    }
+
+    // Konstruktor berparameter
+    public Mahasiswa15(String nm, String nim, double ipk, String kls) {
+        this.nama = nm;
+        this.nim = nim;
+        this.ipk = ipk;
+        this.kelas = kls;
+    }
+
     void tampilkanInformasi() {
         System.out.println("Nama: " + nama);
         System.out.println("NIM: " + nim);
@@ -11,57 +23,22 @@ public class Mahasiswa15 {
         System.out.println("Kelas: " + kelas);
     }
 
-    void ubahKelas(String kelasBaru) {
-        kelas = kelasBaru;
-    }
-
-    void updateIpk(double ipkBaru) {
-        if (ipkBaru >= 0.0 && ipkBaru <= 4.0) {
-            ipk = ipkBaru;
-        } else {
-            System.out.println("IPK tidak valid. Harus antara 0.0 dan 4.0");
-        }
-    }
-
-    String nilaiKinerja() {
-        if (ipk >= 3.5) {
-            return "Kinerja sangat baik";
-        } else if (ipk >= 3.0) {
-            return "Kinerja baik";
-        } else if (ipk >= 2.0) {
-            return "Kinerja cukup";
-        } else {
-            return "Kinerja kurang";
-        }
-    }
-
     public static void main(String[] args) {
-        Mahasiswa15 mhs = new Mahasiswa15();
-        mhs.nama = "Budi";
-        mhs.nim = "12345678";
-        mhs.kelas = "TI-A";
-        mhs.ipk = 3.7;
 
-        System.out.println("===== Data Awal =====");
-        mhs.tampilkanInformasi();
-        System.out.println("Kinerja: " + mhs.nilaiKinerja());
+        // Menggunakan konstruktor default
+        Mahasiswa15 mhs1 = new Mahasiswa15();
+        mhs1.nama = "Budi";
+        mhs1.nim = "12345678";
+        mhs1.ipk = 3.5;
+        mhs1.kelas = "TI-A";
 
-        System.out.println("\n===== Update IPK Valid (3.5) =====");
-        mhs.updateIpk(3.5);
-        System.out.println("IPK setelah diupdate: " + mhs.ipk);
-        System.out.println("Kinerja: " + mhs.nilaiKinerja());
+        System.out.println("=== Data Mahasiswa 1 ===");
+        mhs1.tampilkanInformasi();
 
-        System.out.println("\n===== Update IPK Tidak Valid (5.0) =====");
-        mhs.updateIpk(5.0);
-        System.out.println("IPK setelah diupdate: " + mhs.ipk);
+        // Menggunakan konstruktor berparameter
+        Mahasiswa15 mhs2 = new Mahasiswa15("Siti", "87654321", 3.8, "TI-B");
 
-        System.out.println("\n===== Update IPK Tidak Valid (-1.0) =====");
-        mhs.updateIpk(-1.0);
-        System.out.println("IPK setelah diupdate: " + mhs.ipk);
-
-        System.out.println("\n===== Ubah Kelas =====");
-        mhs.ubahKelas("TI-B");
-        mhs.tampilkanInformasi();
+        System.out.println("\n=== Data Mahasiswa 2 ===");
+        mhs2.tampilkanInformasi();
     }
 }
-
